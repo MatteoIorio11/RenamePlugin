82061915d453c745d3a3a4c196b241ec1afbd1de
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-20 12:15:59 +0100

    feat: init plugin logic, get last commit name

--- RenamePlugin/src/main/kotlin/org/iorio/action/RenameCommitAction.kt
+++ RenamePlugin/src/main/kotlin/org/iorio/action/RenameCommitAction.kt
@@ -3,10 +3,27 @@ package org.iorio.action
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vcs.FilePath
+import com.intellij.openapi.vcs.LocalFilePath
+import com.intellij.openapi.vcs.changes.ui.AsyncChangesTreeImpl.FilePaths
+import java.io.File
 
 class RenameCommitAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project
-        Messages.showInfoMessage(project, "Title", "Content")
+        val name = project?.basePath?.let { lastCommitName(it) }
+    }
+
+    private fun lastCommitName(path: String): String {
+        val command = "git log -1 --pretty=format:%s"
+        return this.executeCommad(command, File(path))
+    }
+
+    private fun executeCommad(command: String, workingDir: File): String {
+        val process = ProcessBuilder(command.split(" "))
+            .directory(workingDir)
+            .redirectErrorStream(true)
+            .start()
+        return process.inputStream.bufferedReader().use { it.readText() }
     }
 }
