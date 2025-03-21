88c0f368ee4b2077fac80d143dda42b1901809db
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-21 09:46:56 +0100

    feat: add gitutils implementation

--- RenamePlugin/src/main/kotlin/org/iorio/action/GitUtils.kt
+++ RenamePlugin/src/main/kotlin/org/iorio/action/GitUtils.kt
@@ -1,21 +1,30 @@
 package org.iorio.action
 
+import com.intellij.util.containers.stream
 import java.io.File
 
 object GitUtils {
     fun isRepository(path: String): Boolean {
-        return false
+        return File(path).listFiles()
+            .stream()
+            .anyMatch{ f: File -> f.isDirectory and f.name.equals(".git")}
     }
 
     fun lastCommitName(path: String): String {
-        return ""
+        val command = listOf("git", "log", "-1", "--pretty=format:%s")
+        return executeCommand(command, File(path))
     }
 
-    fun changeLastCommitName(newName: String, path: String) {
-
+    fun changeLastCommitName(newName: String, path: String): String {
+        val command = listOf("git", "commit", "--amend", "-m", newName)
+        return executeCommand(command, File(path))
     }
 
-    private fun executeCommand(command: List<String>, workingDir: File) {
-
+    private fun executeCommand(command: List<String>, workingDir: File): String {
+        val process = ProcessBuilder(command)
+            .directory(workingDir)
+            .redirectErrorStream(true)
+            .start()
+        return process.inputStream.bufferedReader().use { it.readText() }.trim()
     }
 }
